c2f08452ac5742e239190260086bf208f21ea6d8
Nick Gully <nick@gully.org>
2022-01-26 22:04:24 -0700

    Test fixed, all should pass

--- test/test.cpp
+++ test/test.cpp
@@ -6,8 +6,8 @@ static int Factorial( int number ) {
 // return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
 }
 
-TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
-    REQUIRE( Factorial(0) == 1 );
+TEST_CASE( "Factorial of 0 is 0 (pass)", "[single-file]" ) {
+    REQUIRE( Factorial(0) == 0 );
 }
 
 TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
