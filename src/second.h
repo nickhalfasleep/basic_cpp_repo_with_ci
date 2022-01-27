5c5f438acb6eec029ed7596f3cfe0fd231221806
Nick Gully <nick@gully.org>
2022-01-27 05:56:15 -0700

    Updated Catch2 forward to avoid constexpr issue on fedora.

--- src/second.h
+++ src/second.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <string>
+#include <iostream>
 class second {
 public:
-    second() = default;
+    second() {
+        std::cout << "Constructor\n";
+    }
+
+    ~second() {
+        std::cout << "Destructor!\n";
+    }
     auto second_sight() -> void;
     auto make_it_fourty_two() -> int;
 
